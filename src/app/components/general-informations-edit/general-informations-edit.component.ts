b188e32e31825e3a1264dde91f86e7f1faf5e24d
Camila Kfouri <kfouri.camila@gmail.com>
2025-05-20 15:30:24 +0100

    refactor: move HTTP calls to general-informations-container-component and render filtered lists in general-informations-edit.component

--- src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Form, FormControl, FormGroup } from '@angular/forms';
+import { CountriesList } from '../../types/countries.list';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { StatesList } from '../../types/states-list';
 
 @Component({
   selector: 'app-general-informations-edit',
@@ -7,10 +10,73 @@ import { Form, FormControl, FormGroup } from '@angular/forms';
   templateUrl: './general-informations-edit.component.html',
   styleUrl: './general-informations-edit.component.scss'
 })
-export class GeneralInformationsEditComponent {
+export class GeneralInformationsEditComponent implements OnInit, OnChanges {
+
+  countriesListFiltered: CountriesList = [];
+  statesListFiltered: StatesList = [];
+
+
+
   @Input({ required: true }) userFormEdit!: FormGroup;
+
+  @Input({ required: true }) countriesListEdit: CountriesList = [];
+  @Input({ required: true }) statesListEdit: StatesList = [];
+
+  @Output() onCountrySelectedEmitt = new EventEmitter<string>();
+
+
+  
+  ngOnInit() {
+    this.watchCountryFormChangesAndFilter();
+    this.watchStateFormChangesAndFilter();
+  }
+  
+  ngOnChanges(changes: SimpleChanges) {
+    this.countriesListFiltered = this.countriesListEdit;
+    this.statesListFiltered = this.statesListEdit;
+  }
 
   get emailControl(): FormControl {
     return this.userFormEdit.get('generalInformations.email') as FormControl;
+  }
+
+  get countryControl(): FormControl {
+    return this.userFormEdit.get('generalInformations.country') as FormControl;
+  }
+
+  get stateControl(): FormControl {
+    return this.userFormEdit.get('generalInformations.state') as FormControl;
+  }
+
+
+
+  onCountrySelected(event: MatAutocompleteSelectedEvent) {
+    this.onCountrySelectedEmitt.emit(event.option.value);
+  }
+
+  onStateSelected($event: MatAutocompleteSelectedEvent) {
+
+  }
+
+
+  private watchCountryFormChangesAndFilter() {
+    // this.countryControl.valueChanges.subscribe((country: string) => this.filterCountryList(country));
+    this.countryControl.valueChanges.subscribe(this.filterCountriesList.bind(this));
+  }
+
+  private filterCountriesList(searchTerm: string) {
+    this.countriesListFiltered = this.countriesListEdit.filter(
+      (country) => country.name.toLowerCase().includes(searchTerm.toLowerCase().trim()));
+  }
+
+
+
+  private watchStateFormChangesAndFilter() {
+    this.stateControl.valueChanges.subscribe(this.filterStatesList.bind(this));
+  }
+
+  private filterStatesList(searchTerm: string) {
+    this.statesListFiltered = this.statesListEdit.filter(
+      (state) => state.name.toLowerCase().includes(searchTerm.toLowerCase().trim()));
   }
 }
--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,6 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { IUser } from '../../interfaces/user/i-user';
 import { UserFormController } from './user-form-controller';
+import { CountriesService } from '../../services/countries.service';
+import { CountriesList } from '../../types/countries.list';
+import { take } from 'rxjs';
+import { StatesService } from '../../services/states.service';
+import { StatesList } from '../../types/states-list';
 
 @Component({
   selector: 'app-user-informations-container',
@@ -8,11 +13,21 @@ import { UserFormController } from './user-form-controller';
   templateUrl: './user-informations-container.component.html',
   styleUrl: './user-informations-container.component.scss'
 })
-export class UserInformationsContainerComponent extends UserFormController implements OnChanges{
+export class UserInformationsContainerComponent extends UserFormController implements OnInit, OnChanges{
+
   currentTabIndex: number = 0;
+  countriesList: CountriesList = [];
+  StatesListContainerC: StatesList = [];
+
+  private readonly _countriesService = inject(CountriesService);
+  private readonly _statesService = inject(StatesService);
 
   @Input({ required: true }) isInEditModeInformationContainerC: boolean = false;
   @Input({ required: true }) userSelectedInformationContainerC: IUser = {} as IUser;
+
+  ngOnInit(){
+    this.getCountriesList();
+  }
 
   ngOnChanges(changes: SimpleChanges){
     this.currentTabIndex = 0;
@@ -21,6 +36,23 @@ export class UserInformationsContainerComponent extends UserFormController imple
 
     if (HAS_USER_SELECTED) {
       this.fulfillForm(this.userSelectedInformationContainerC);
+      this.getSatesList(this.userSelectedInformationContainerC.country);
     }
+  }
+  
+  getCountriesList() {
+    this._countriesService.getCountries().pipe(take(1)).subscribe((countriesList: CountriesList) => {
+      this.countriesList = countriesList;
+    });
+  }
+
+  onCountrySelected(countryName: string) {
+    this.getSatesList(countryName);
+  }
+
+  private getSatesList(country: string) {
+    this._statesService.getStates(country).pipe(take(1)).subscribe((statesList: StatesList) => {
+      this.StatesListContainerC = statesList;
+    });
   }
 }
